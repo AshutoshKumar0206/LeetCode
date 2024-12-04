131a8278ad98ab9ed93eed532604bdcb11404ed0
Ashutosh Kumar <123981820+AshutoshKumar0206@users.noreply.github.com>
2024-12-04 20:16:35 +0530

    [473] [Time Beats: 41.73%] [Memory Beats: 39.33%] - LeetPush

---
+++ 473-Matchsticks-to-Square.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    private:
+    bool formSquare(int index, int sum_sides, int req_equal_sides, int square_sides,
+    vector<int>& matchsticks, vector<int>&vis){
+        if(square_sides == 1){
+            return true;
+        }
+        if(sum_sides == req_equal_sides){
+            return formSquare(0, 0, req_equal_sides, square_sides-1, matchsticks, vis);
+        }
+        for(int j = index; j<matchsticks.size(); j++){
+            if(vis[j] || (sum_sides + matchsticks[j] > req_equal_sides)) continue;
+            vis[j] = 1;
+            if(formSquare(j+1, sum_sides + matchsticks[j], req_equal_sides,
+            square_sides, matchsticks, vis))return true;
+            vis[j] = 0;
+        }
+        return false;
+    }
+public:
+    bool makesquare(vector<int>& matchsticks) {
+        long long total_matchsticks = accumulate(matchsticks.begin(), matchsticks.end(), 0);
+        int req_equal_sides = total_matchsticks/4;
+        int square_sides = 4;
+        if(matchsticks.size()<4 || total_matchsticks % 4) return false;
+        vector<int>vis(matchsticks.size(), 0);
+        return formSquare(0, 0, req_equal_sides, square_sides, matchsticks, vis);
+    }
+};
