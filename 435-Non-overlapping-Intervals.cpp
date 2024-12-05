9e9d7e1f71fcd31daf4d71674c8a56669ca1c583
Ashutosh Kumar <123981820+AshutoshKumar0206@users.noreply.github.com>
2024-12-05 11:17:30 +0530

    [435] [Time Beats: 19.16%] [Memory Beats: 25.98%] - LeetPush

--- 435-Non-overlapping-Intervals.cpp
+++ 435-Non-overlapping-Intervals.cpp
@@ -1,23 +1,28 @@
 class Solution {
-//Sorting
+//Tabulation and Binary Search
     private:
-    static bool cmp(vector<int>&a, vector<int>&b){
-        return a[1]<b[1];
+    int findNextMin(int i, int n, vector<vector<int>>&intervals){
+        int l = i, r = n;
+        while(l+1<r){
+            int m = l+(r-l)/2;
+            if(intervals[m][0] < intervals[i][1]){
+                l = m;
+            }
+            else{
+                r = m;
+            }
+        }
+        return r;
     }
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
     int n = intervals.size();
-    sort(intervals.begin(), intervals.end(), cmp);
-    int prev = intervals[0][1];
-    int cnt = 0; 
-    for(int i = 1; i<n; i++){
-        if(prev > intervals[i][0]){
-            cnt++;
-        }
-        else{
-            prev = intervals[i][1];
-        }
-    }
-    return cnt;
+    sort(intervals.begin(), intervals.end());
+   vector<int>dp(n+1, 0);
+   for(int i = n-1; i>=0; i--){
+    int j = findNextMin(i, n, intervals);
+    dp[i] = max(1+dp[j], dp[i+1]);
+   }
+   return n-dp[0];
     }
 };
